5e41996b2ed516730359a1ebeeef4c85c489564f
Fabricio Vieira <44281970+fabricio027@users.noreply.github.com>
2024-08-22 04:41:35 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>Ana Maria, você e a Laura são meus tesouros, amo vocês. Se você me ama, dá uma risadinha... 🤓</h1>;
+  return (
+    <h1>
+      Ana Maria, você e a Laura são meus tesouros, amo vocês. Se você me ama, dá
+      uma risadinha... 🤓
+    </h1>
+  );
 }
 
 export default Home;
